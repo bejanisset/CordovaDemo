9ca7445ac477f2e961c2924e01f1f46e15983d88
Janisset <janisset-benoit@bbox.fr>
2018-03-15 16:21:59 +0100

    Camera service test

--- www/js/services/camera.js
+++ www/js/services/camera.js
@@ -2,7 +2,17 @@ angular.module('starter.controllers')
     .service('camera', function() {
     console.log('init service');
     this.TakePicture = function(ms) {
-        camera.getPicture();
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+        function onSuccess(imageURI) {
+            var image = document.getElementById('myImage');
+            image.src = imageURI;
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
     };
     
 });
