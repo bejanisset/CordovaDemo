af16f22a421a23279f7fa6830dd53e5accf8e4a2
Janisset <janisset-benoit@bbox.fr>
2018-03-21 10:49:47 +0100

    Test Camera DATA_URL 8

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,13 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
+
   
 })
 
 
 .controller('VibrateCtrl', function($scope, vibration) {
+    navigator.splashscreen.show();
     console.log("Vibrate");
     $scope.vibration = vibration;
 })
