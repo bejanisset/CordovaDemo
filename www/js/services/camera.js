7b2332e07234d9693cd296db1a2caa01f43f7dc0
Janisset <janisset-benoit@bbox.fr>
2018-03-21 10:11:32 +0100

    Test Camera DATA_URL 7

--- www/js/services/camera.js
+++ www/js/services/camera.js
@@ -7,17 +7,13 @@ angular.module('starter.controllers')
     this.TakePicture = function() {
         navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
     destinationType: Camera.DestinationType.DATA_URL });
-
         function onSuccess(imageData) {
             var image =  document.getElementById('myImage') ;
             image.style.display = "block";
             image.src = "data:image/jpeg;base64," + imageData;
         }
-
         function onFail(message) {
             alert (' a échoué car: '+ message);
         }
-        
     };
-    
 });
