3abf89367c4fc70c717203d1e3cc17573c5d54c4
Janisset <janisset-benoit@bbox.fr>
2018-03-21 09:03:24 +0100

    Test Camera DATA_URL 2

--- www/js/services/camera.js
+++ www/js/services/camera.js
@@ -6,11 +6,11 @@ angular.module('starter.controllers')
     
     this.TakePicture = function() {
         navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-    destinationType: Camera.DestinationType.DATA_URL });
+    destinationType: Camera.DestinationType.DATA_URL, encodingType: Camera.EncodingType.JPEG });
 
         function onSuccess(imageURI) {
             var image = document.getElementById('myImage');
-            image.src = imageURI;
+            image.src = "data:image/jpeg;base64," + imageData;
         }
 
         function onFail(message) {
